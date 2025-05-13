f7051fc6ed54ccc195969af9bcd3e57d48c30c25
karrk <101902544+karrk@users.noreply.github.com>
2025-05-14 00:22:07 +0900

    [Feat] 기본 베이스 팝업 기능 제작

---
+++ Assets/1.Worker/Interface/PointEvtHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointEvtHandler : MonoBehaviour
+    , IPointerUpHandler, IPointerDownHandler, IDragHandler, IPointerEnterHandler,
+    IPointerExitHandler, IBeginDragHandler, IEndDragHandler
+{
+    public Image Img { get; private set; }
+    public Action<PointerEventData> OnClickDownHandler;
+    public Action<PointerEventData> OnClickUpHandler;
+    public Action<PointerEventData> OnDragHandler;
+
+    public Action<PointerEventData> OnPointerEnterHandler;
+    public Action<PointerEventData> OnPointerExitHandler;
+
+    public Action<PointerEventData> OnDragBeginHandler;
+    public Action<PointerEventData> OnDragEndHandler;
+
+    private void Awake()
+    {
+        Img = GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        OnDragHandler?.Invoke(eventData);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnClickDownHandler?.Invoke(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OnClickUpHandler?.Invoke(eventData);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnPointerEnterHandler?.Invoke(eventData);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnPointerExitHandler?.Invoke(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnDragEndHandler?.Invoke(eventData);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        OnDragBeginHandler?.Invoke(eventData);
+    }
+}
